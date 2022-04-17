cf9769fcbf219fda581996ca8bfbf597e602b47c
sarkadv <101476511+sarkadv@users.noreply.github.com>
2022-04-17 16:55:30 +0200

    vylepseni vytvareni nove mapy

--- src/view/NewMapWindow.java
+++ src/view/NewMapWindow.java
@@ -1,6 +1,9 @@
 package view;
 
+import java.util.List;
+
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
@@ -8,6 +11,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.control.Spinner;
@@ -29,24 +33,32 @@ public class NewMapWindow extends Stage {
 	private MapModel model;
 	private TextField mapSizeWidthTF;
 	private TextField mapSizeHeightTF;
-	private ChoiceBox<Integer> tilesVisibleChoice;
+	private ComboBox<Integer> tilesVisibleCombo;
+	private List<Integer> tilesVisibleChoiceList;
+	private Label mapSizeComputedNumber;
+	private final int MAX_TILE_NUMBER = 1600;
+	private int chosenTilesVisible;
 	
 	public NewMapWindow(MapModel model, Stage primaryStage) {
 		this.model = model;
+		this.mapSizeWidthTF = new TextField("8");
+		this.mapSizeHeightTF = new TextField("8");
+		this.mapSizeComputedNumber = new Label("64");
+		this.tilesVisibleChoiceList = FXCollections.observableArrayList(1, 4, 9, 16, 25, 36, 49, 64);
 		
 		this.setScene(createScene());
 		this.initModality(Modality.WINDOW_MODAL);
 		this.setTitle("Nová Mapa");
 		this.initOwner(primaryStage);
 		this.setOnCloseRequest(e -> e.consume());
-		this.setMinHeight(250);
-		this.setMaxHeight(250);
+		this.setMinHeight(275);
+		this.setMaxHeight(275);
 		this.setMinWidth(400);
 		this.setMaxWidth(400);
 	}
 	
 	private Scene createScene() {
-		Scene scene = new Scene(createRootPane(), 400, 250);
+		Scene scene = new Scene(createRootPane(), 400, 300);
 		
 		return scene;
 	}
@@ -73,27 +85,36 @@ public class NewMapWindow extends Stage {
 		HBox mapSizeControls = new HBox(5);
 		mapSizeControls.setAlignment(Pos.CENTER);
 		mapSizeControls.setPadding(new Insets(10));
+		
 		Label mapSizeWidthLabel = new Label("Šířka: ");
-		mapSizeWidthTF = new TextField("8");
 		mapSizeWidthTF.setPrefColumnCount(3);
 		mapSizeWidthTF.textProperty().addListener((obs, oldValue, newValue) -> {
             if(!newValue.matches("\\d*")){
             	mapSizeWidthTF.setText(oldValue);
             }
+            computeMapSize();
+            computeTilesVisibleChoiceList();
 		});
 		
 		Label mapSizeHeightLabel = new Label("Výška: ");
-		mapSizeHeightTF = new TextField("8");
 		mapSizeHeightTF.setPrefColumnCount(3);
 		mapSizeHeightTF.textProperty().addListener((obs, oldValue, newValue) -> {
             if(!newValue.matches("\\d*")){
             	mapSizeHeightTF.setText(oldValue);
             }
+            computeMapSize();
+            computeTilesVisibleChoiceList();
 		});
 		
 		mapSizeControls.getChildren().addAll(mapSizeWidthLabel, mapSizeWidthTF, mapSizeHeightLabel, mapSizeHeightTF);
 		
-		mapSizePane.getChildren().addAll(mapSizeLabel, mapSizeControls);
+		HBox mapSizeComputedPane = new HBox(5);
+		mapSizeComputedPane.setAlignment(Pos.CENTER);
+		Label mapSizeComputedLabel = new Label("Počet dlaždic: ");
+		
+		mapSizeComputedPane.getChildren().addAll(mapSizeComputedLabel, mapSizeComputedNumber);
+		
+		mapSizePane.getChildren().addAll(mapSizeLabel, mapSizeControls, mapSizeComputedPane);
 		
 		return mapSizePane;
 	}
@@ -103,10 +124,11 @@ public class NewMapWindow extends Stage {
 		visibleTilesPane.setPadding(new Insets(10));
 		
 		Label visibleTilesLabel = new Label("Počet Viditelných Dlaždic");
-		tilesVisibleChoice = new ChoiceBox<Integer>();
-		tilesVisibleChoice.setItems(FXCollections.observableArrayList(1, 4, 9, 16, 25, 36, 49, 64, 81, 100));
-		tilesVisibleChoice.setValue(16);
-		visibleTilesPane.getChildren().addAll(visibleTilesLabel, tilesVisibleChoice);
+		tilesVisibleCombo = new ComboBox<Integer>();
+		tilesVisibleCombo.setItems(FXCollections.observableArrayList(tilesVisibleChoiceList));
+		tilesVisibleCombo.setValue(16);
+		
+		visibleTilesPane.getChildren().addAll(visibleTilesLabel, tilesVisibleCombo);
 		
 		return visibleTilesPane;
 	}
@@ -120,20 +142,18 @@ public class NewMapWindow extends Stage {
 			alert.showAndWait();
 			
 		}
+		else if(Integer.parseInt(mapSizeComputedNumber.getText()) > MAX_TILE_NUMBER){
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Nová mapa");
+			alert.setHeaderText("Nepodařilo se vytvořit novou mapu");
+			alert.setContentText("Celkový počet dlaždic nesmí být větší než " + MAX_TILE_NUMBER + " .");
+			alert.showAndWait();
+		}
 		else {
 			int allTilesWidth = Integer.parseInt(mapSizeWidthTF.getText());
 			int allTilesHeight = Integer.parseInt(mapSizeHeightTF.getText());
 			
-			int tilesVisibleLine = (int)Math.sqrt(tilesVisibleChoice.getValue());
-			int smallerSide = Math.min(allTilesHeight, allTilesWidth);
-			if(tilesVisibleLine > smallerSide) {
-				tilesVisibleLine = smallerSide;
-				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Nová mapa");
-				alert.setHeaderText("Počet viditelných dlaždic byl upraven");
-				alert.setContentText("Počet viditelných dlaždic nesmí být větší než rozměry mapy.");
-				alert.showAndWait();
-			}
+			int tilesVisibleLine = (int)Math.sqrt(tilesVisibleCombo.getValue());
 			
 			model.setAllTilesWidth(allTilesWidth);
 			model.setAllTilesHeight(allTilesHeight);
@@ -142,5 +162,36 @@ public class NewMapWindow extends Stage {
 			this.close();
 		}
 		
+	}
+	
+	private void computeMapSize() {
+		if(!mapSizeHeightTF.getText().equals("") && !mapSizeWidthTF.getText().equals("")) {
+			int value = Integer.parseInt(mapSizeHeightTF.getText()) * Integer.parseInt(mapSizeWidthTF.getText());
+			mapSizeComputedNumber.setText(String.valueOf(value));
+		}
+		else {
+			mapSizeComputedNumber.setText("");
+		}
+		
+	}
+	
+	private void computeTilesVisibleChoiceList() {
+		int chosenTilesVisible = tilesVisibleCombo.getValue();
+		
+		if(!mapSizeHeightTF.getText().equals("") && !mapSizeWidthTF.getText().equals("")) {
+			int smallerSide = Math.min(Integer.parseInt(mapSizeWidthTF.getText()), Integer.parseInt(mapSizeHeightTF.getText()));
+			
+			this.tilesVisibleChoiceList = FXCollections.observableArrayList();
+			
+			for(int i = 1; i <= smallerSide; i++) {
+				tilesVisibleChoiceList.add(i*i);
+			}
+			
+			this.tilesVisibleCombo.setItems(FXCollections.observableArrayList(tilesVisibleChoiceList));
+		
+			if(Math.sqrt(chosenTilesVisible) > smallerSide) {
+				this.tilesVisibleCombo.setValue(smallerSide*smallerSide);
+			}
+		}	
 	}
 }
